6308b49d00c324684beaad100a8644b573f06ca4
Álvaro Pérez <alvarojperezperez@gmail.com>
2026-09-11 17:14:06 +0200

    Set foldlevelstart to 99 so buffers open with all folds open

--- init.lua
+++ init.lua
@@ -167,6 +167,11 @@ do
   -- Minimal number of screen lines to keep above and below the cursor.
   vim.o.scrolloff = 10
 
+  -- Start editing every buffer with all folds open. Folds themselves are
+  -- defined by treesitter, this only sets their initial state.
+  -- See `:help 'foldlevelstart'`
+  vim.o.foldlevelstart = 99
+
   -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
   -- instead raise a dialog asking if you wish to save the current file(s)
   -- See `:help 'confirm'`
