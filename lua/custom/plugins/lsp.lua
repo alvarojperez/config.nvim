97d9c8e01b7cdea4bbc52b2c2d6efbe92dea4d43
Álvaro Pérez <alvarojperezperez@gmail.com>
2026-09-12 15:35:52 +0200

    Fix partial augroup rename bug

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -52,10 +52,10 @@ vim.api.nvim_create_autocmd('LspAttach', {
       })
 
       vim.api.nvim_create_autocmd('LspDetach', {
-        group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+        group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
         callback = function(event2)
           vim.lsp.buf.clear_references()
-          vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+          vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
         end,
       })
     end
