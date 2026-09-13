96daf4a662ffa1175c08a14977ece9ad1135bcfd
Álvaro Pérez <alvarojperezperez@gmail.com>
2026-09-13 19:06:27 +0200

    Fix undefined MiniIcons when loading only ui.lua

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -18,7 +18,7 @@ if vim.g.have_nerd_font then
   vim.pack.add { 'https://github.com/nvim-mini/mini.icons' }
   require('mini.icons').setup()
   -- Used for backwards compatibility with plugins that require `nvim-web-devicons` (e.g. telescope.nvim)
-  MiniIcons.mock_nvim_web_devicons()
+  require('mini.icons').mock_nvim_web_devicons()
 end
 
 -- Simple and easy statusline.
