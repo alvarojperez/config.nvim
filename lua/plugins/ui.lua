cac43dd8958d717203286efd85190b40ad641e62
Álvaro Pérez <alvarojperezperez@gmail.com>
2026-09-17 17:05:01 +0200

    Change git keybinds to <leader>g

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -17,7 +17,7 @@ require('which-key').setup {
   spec = {
     { '<leader>s', group = '[S]earch', icon = '', mode = { 'n', 'v' } },
     { '<leader>t', group = '[T]oggle' },
-    { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } }, -- Enable gitsigns recommended keymaps first
+    { '<leader>g', group = '[G]it', icon = { cat = 'filetype', name = 'git', color = 'orange' }, mode = { 'n', 'v' } }, -- Enable gitsigns recommended keymaps first
     { 'gr', group = 'LSP Actions', mode = { 'n' } },
   },
 }
