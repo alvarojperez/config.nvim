f3bbbc3d7fa11cfcbbe62e551e414936fa075e48
Álvaro Pérez <alvarojperezperez@gmail.com>
2026-09-25 15:58:25 +0200

    Cleanup telescope config

--- lua/plugins/picker.lua
+++ lua/plugins/picker.lua
@@ -1,27 +1,3 @@
--- [[ Fuzzy Finder (files, lsp, etc) ]]
---
--- Telescope is a fuzzy finder that comes with a lot of different things that
--- it can fuzzy find! It's more than just a "file finder", it can search
--- many different aspects of Neovim, your workspace, LSP, and more!
---
--- There are lots of other alternative pickers (like snacks.picker, or fzf-lua)
--- so feel free to experiment and see what you like!
---
--- The easiest way to use Telescope, is to start by doing something like:
---  :Telescope help_tags
---
--- After running this command, a window will open up and you're able to
--- type in the prompt window. You'll see a list of `help_tags` options and
--- a corresponding preview of the help.
---
--- Two important keymaps to use while in Telescope are:
---  - Insert mode: <c-/>
---  - Normal mode: ?
---
--- This opens a window that shows you all of the keymaps for the current
--- Telescope picker. This is really useful to discover what Telescope can
--- do as well as how to actually do it!
-
 ---@type (string|vim.pack.Spec)[]
 local telescope_plugins = {
   'https://github.com/nvim-lua/plenary.nvim',
@@ -30,20 +6,10 @@ local telescope_plugins = {
 }
 if vim.fn.executable 'make' == 1 then table.insert(telescope_plugins, 'https://github.com/nvim-telescope/telescope-fzf-native.nvim') end
 
--- NOTE: You can install multiple plugins at once
 vim.pack.add(telescope_plugins)
 
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
-  -- You can put your default mappings / updates / etc. in here
-  --  All the info you're looking for is in `:help telescope.setup()`
-  --
-  -- defaults = {
-  --   mappings = {
-  --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-  --   },
-  -- },
-  -- pickers = {}
   extensions = {
     ['ui-select'] = { require('telescope.themes').get_dropdown() },
   },
